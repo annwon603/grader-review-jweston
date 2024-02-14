2adbae8540849479165d4852637a6964836b9cde
jak-weston <jak.w.weston@gmail.com>
2024-02-14 15:00:46 -0800

    w

--- grade.sh
+++ grade.sh
@@ -6,7 +6,7 @@ rm -rf git-output.txt
 
 mkdir grading-area
 
-git clone $1 student-submission 8> git-output.txt
+git clone $1 student-submission 2> git-output.txt
 echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
@@ -20,6 +20,8 @@ if [[ -f student-submission/ListExamples.java ]]
 then
     cp student-submission/ListExamples.java grading-area/
     cp TestListExamples.java grading-area/
+    cp -r lib grading-area/
+    echo "File found!"
 else
     echo "Missing student-submission/ListExamples.java, did you forget the file or misname it?"
     exit 1
@@ -34,17 +36,17 @@ pwd
 javac -cp $CPATH *.java
 
 #check for comple errors
-if [[ $? -ne 0 ]];
+if [[ $? -ne 0 ]]
 then
     echo "The program failed to comple, see compile error above"
     exit 1
 fi
 
 #run the tester with Junit
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
 
-lastline = $(cat grading/junit.output.txt | tail -n 2 | head -n)1 
+lastline=$(cat junit-output.txt | tail -n 2 | head -1) 
 tests=$(echo $lastline |awk -F '[, ]' '{print $3}')
 failures=$(echo $lastline | awk -F '(, )' '{print $6}')
 sucesses=$((tests-failures))
-echo "your score is $sucesses / $tests
+echo "your score is $sucesses / $tests"
